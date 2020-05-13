8217fd0bc980633d5dd52d17a0f3936ca6bb5d6f
zellitsss <zellitsss@gmail.com>
2020-05-13 15:57:36 +0700

    + Render bugs from list

--- src/App.js
+++ src/App.js
@@ -2,22 +2,61 @@ import React from 'react';
 import './App.css';
 import Bug from './components/Bug';
 
-const data = {
-  status: 0,
-  name: 'Aliqua commodo veniam Lorem reprehenderit consequat esse deserunt.',
-  description: 'Anim adipisicing amet elit exercitation voluptate. Aute adipisicing adipisicing elit dolor ullamco veniam quis elit ea aute. Nisi culpa dolor officia aliqua mollit sunt pariatur occaecat anim incididunt. Sint occaecat magna ipsum duis. Duis fugiat deserunt in minim excepteur commodo consectetur aute eu. Officia consequat aute anim reprehenderit cillum minim.'
-};
+const bugs = [
+  {
+    id: 1,
+    status: 0,
+    name: 'Ut cillum dolore sit et occaecat pariatur mollit occaecat culpa excepteur tempor ut pariatur.',
+    description: 'Voluptate occaecat enim incididunt cillum. Cupidatat pariatur nisi minim consequat excepteur culpa elit non tempor. Ut et consectetur est minim cillum veniam est esse amet et anim excepteur amet est. Labore nisi tempor officia deserunt.'
+  },
+  {
+    id: 2,
+    status: 0,
+    name: 'Ea enim esse anim aliquip sint anim consequat in commodo consectetur do aute ullamco.',
+    description: 'Irure est exercitation id commodo aliqua et esse cillum eu adipisicing est aliquip do deserunt. Minim minim laborum fugiat qui incididunt adipisicing amet do reprehenderit. Non excepteur officia ad anim laborum tempor fugiat in. Magna labore id non aliqua nulla magna sit minim ipsum irure consequat. Ex ut elit pariatur cupidatat dolor. Est nulla aute amet ipsum.'
+  },
+  {
+    id: 3,
+    status: 1,
+    name: 'Nulla adipisicing deserunt aliquip pariatur laboris sunt duis anim occaecat dolore consectetur reprehenderit laborum proident.',
+    description: 'Culpa consequat ullamco duis laboris elit ullamco qui reprehenderit quis. Occaecat sit ex dolore cillum quis anim commodo dolor deserunt est. Adipisicing do sint adipisicing sint qui ad.'
+  },
+  {
+    id: 4,
+    status: 0,
+    name: 'Nostrud adipisicing id quis proident dolore do occaecat magna sint tempor sunt et voluptate velit.',
+    description: 'Amet cillum amet duis cillum sunt occaecat anim nostrud. Sint est consequat qui nulla occaecat minim proident incididunt cillum sit. In dolore minim qui ad ex officia sit anim reprehenderit et. Pariatur ipsum commodo amet esse enim occaecat aute qui sunt consectetur. Minim id labore magna reprehenderit enim labore anim laboris aliquip elit. Commodo nisi ea enim veniam minim cupidatat labore nulla occaecat et velit aute non.'
+  },
+  {
+    id: 5,
+    status: 1,
+    name: 'Dolor ea pariatur qui exercitation ex mollit quis.',
+    description: 'Pariatur ea officia in do. Qui fugiat non elit sunt sint anim sint commodo id elit laboris nisi. Eu ad sint ipsum nulla voluptate aute et consequat consectetur voluptate esse. Quis eiusmod veniam aliquip cillum adipisicing laboris culpa laborum minim ea.'
+  },
+  {
+    id: 6,
+    status: 0,
+    name: 'Nisi quis duis pariatur ut fugiat laboris Lorem Lorem voluptate id qui.',
+    description: 'Minim ex sint excepteur tempor culpa culpa consequat labore adipisicing minim exercitation aliqua officia. Anim ut consectetur duis anim deserunt anim occaecat exercitation cupidatat quis consectetur amet. Laborum irure ex non ea voluptate officia eu cupidatat id ea dolor.'
+  },
+]
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.bugs = bugs;
+  }
   render() {
+    let bugsList = this.bugs.map((bug) =>
+      <Bug data={bug} key={bug.id.toString()} />
+    );
     return (
       <div className="min-h-screen py-8">
         <div className="max-w-xl mx-auto bg-gray-300 p-4 text-center">
           <header className="app-title">Bugs Tracking</header>
           <section>filter</section>
           <section className="bugs-list">
-            <Bug data={data}/>
-
+            {bugsList}
           </section>
         </div>
       </div>
--- src/components/Bug.js
+++ src/components/Bug.js
@@ -3,21 +3,40 @@ import Badge from './Badge.js';
 import ActionButton from './ActionButton.js';
 
 export default class Bug extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.onHeaderClicked = this.onHeaderClicked.bind(this);
+        this.state = {
+            showDescription: false
+        };
+    }
+
     render() {
+        let descriptionClass = 'bug-description';
+        if (!this.state.showDescription) {
+            descriptionClass += ' hidden';
+        }
         return (
             <div className="bug-item">
-              <div className="bug-header">
+              <div className="bug-header" onClick={this.onHeaderClicked}>
                 <Badge status={this.props.data.status} />{this.props.data.name}
               </div>
-              <div className="bug-description">
+              <div className={descriptionClass}>
                 <div className="text-justify">
                   {this.props.data.description}
                 </div>
-                <div>
+                <div className="pt-2">
                   <ActionButton status={this.props.data.status} />
                 </div>
               </div>
             </div>
         );
+    }
+
+    onHeaderClicked() {
+        this.setState(state => ({
+            showDescription: !state.showDescription
+        }));
     }
 }
