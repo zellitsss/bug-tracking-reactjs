fa2cc0b1208b9a833b5d30c051f29a9f3f2d316b
zellitsss <zellitsss@gmail.com>
2020-05-13 14:43:49 +0700

    + Basic style for app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div class="min-h-screen py-8">
+        <div className="max-w-xl mx-auto bg-gray-300 p-4 text-center">
+          <header className="app-title">Bugs Tracking</header>
+          <section>filter</section>
+          <section className="bugs-list">
+            <div className="bug-item">
+              <div className="bug-header">
+                <span className="bug-badge opened">Opened</span>Aliqua commodo veniam Lorem reprehenderit consequat esse deserunt.
+              </div>
+              <div className="bug-description">
+                <div className="text-justify">
+                  Est dolor occaecat veniam nostrud excepteur incididunt in. Velit deserunt duis consectetur velit laboris officia qui duis dolore sit eu. Occaecat non dolore minim ex ullamco officia sint ea excepteur magna do mollit proident aliquip. Eiusmod nostrud do sit ea duis minim aliqua do duis. Et consequat excepteur esse et elit adipisicing pariatur occaecat deserunt exercitation reprehenderit. Sunt incididunt laborum laborum duis laboris id amet ex id.
+                </div>
+                <div>
+                  <button className="action-btn resolve">Mark as Resolved</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="bug-item">
+              <div className="bug-header">
+                <span className="bug-badge resolved">Resolved</span>Aliqua commodo veniam Lorem reprehenderit consequat esse deserunt.
+              </div>
+              <div className="bug-description">
+                <div className="text-justify">
+                  Est dolor occaecat veniam nostrud excepteur incididunt in. Velit deserunt duis consectetur velit laboris officia qui duis dolore sit eu. Occaecat non dolore minim ex ullamco officia sint ea excepteur magna do mollit proident aliquip. Eiusmod nostrud do sit ea duis minim aliqua do duis. Et consequat excepteur esse et elit adipisicing pariatur occaecat deserunt exercitation reprehenderit. Sunt incididunt laborum laborum duis laboris id amet ex id.
+                </div>
+                <div>
+                  <button className="action-btn reopen">Reopen</button>
+                </div>
+              </div>
+            </div>
+
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
